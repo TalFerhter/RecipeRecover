3579b0458e8ed31b978f8e0f40fc718dc3d23904
Tal Fershter <tal.fershter@gmail.com>
2020-10-06 16:51:39 +0300

    add initialize springboot

---
+++ src/main/java/org/openu/recipeRecover/RecipeRecoverApplication.java
@@ -0,0 +1,13 @@
+package org.openu.recipeRecover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeRecoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeRecoverApplication.class, args);
+	}
+
+}
