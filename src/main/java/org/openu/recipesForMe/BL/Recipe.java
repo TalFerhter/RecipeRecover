f8b76c81c5478ee5bb6f0fd055fa6cd4b0acae1e
Tal Fershter <tal.fershter@gmail.com>
2020-12-01 18:15:27 +0200

    db properties

--- src/main/java/org/openu/recipesForMe/BL/Recipe.java
+++
@@ -1,9 +0,0 @@
-package org.openu.recipesForMe.BL;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "recipes")
-public class Recipe {
-}
--- src/main/java/org/openu/recipesForMe/RecipesForMeApplication.java
+++ src/main/java/org/openu/recipesForMe/RecipesForMeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecipeRecoverApplication {
+public class RecipesForMeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RecipeRecoverApplication.class, args);
+		SpringApplication.run(RecipesForMeApplication.class, args);
 	}
 
 }
